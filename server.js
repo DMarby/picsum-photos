f4b13f6d8b1f6c24962a9a4e7a6cd3eed5ffcbfa
David Marby <david@dmarby.se>
2014-09-10 15:08:13 +0200

    .

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ module.exports = function (callback) {
 
   app.use(express.static(path.join(__dirname, 'public')));
 
-  app.all('/', function(req, res, next) {
+  app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
