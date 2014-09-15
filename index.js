b96caefeac8680fc01f9cd8e8e35c26c34746b6d
David Marby <david@dmarby.se>
2014-09-15 12:05:02 +0200

    .

--- index.js
+++ index.js
@@ -166,8 +166,8 @@ if (cluster.isMaster) {
 
           result.filename = filename;
           result.id = highestImageId++;
-          console.log(path.basename(filename));
           var the_metadata = findMetadata(path.basename(filename));
+          console.log(the_metadata);
           result.post_url = the_metadata.post_url;
           result.author = the_metadata.author;
           result.author_url = the_metadata.author_url;
