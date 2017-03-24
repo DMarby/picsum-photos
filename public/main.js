55c28a2ffc85a15e4ee9cf61e437a9b976f270a9
David Marby <david@dmarby.se>
2017-03-25 00:17:11 +0100

    Replace ws.unsplash.it with ip

--- public/main.js
+++ public/main.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
   var first = true;
   var countingFromZero = true;
 
-  var socket = io.connect('http://ws.unsplash.it:4000');
+  var socket = io.connect('http://45.55.96.43:4000');
   socket.on('stats', function (data) {
     if (!first && countingFromZero) {
       return;
