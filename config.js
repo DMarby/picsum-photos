2a2e7f60ca9b870738c2a4dd9d27e67d77ad8c41
David Marby <david@dmarby.se>
2017-10-29 10:36:55 +0100

    Initial development setup for go

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = exports = function () {
-  var configFile = require('./config.json')
-  var node_env = process.env.NODE_ENV || 'production'
-  var config = configFile[node_env]
-  config.env = node_env
-  return config
-}
