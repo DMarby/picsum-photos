7a0129adf6dde7b4ad8e25f80df0d5f9e05059f1
David Marby <david@dmarby.se>
2018-02-22 05:33:11 +0100

    Add initial gin setup

--- main.go
+++ main.go
@@ -1,9 +1,70 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/DMarby/picsum-photos/api"
+	"github.com/DMarby/picsum-photos/queue"
+	"github.com/oklog/run"
 )
 
+func handleInterrupt(ctx context.Context) error {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case sig := <-c:
+		return fmt.Errorf("received signal %s", sig)
+	case <-ctx.Done():
+		return errors.New("canceled")
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	var g run.Group
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Start worker queue
+	workerQueue := queue.New(ctx, runtime.NumCPU(), func(data interface{}) (interface{}, error) {
+		stringData := data.(string)
+		return stringData, nil
+	})
+
+	g.Add(func() error {
+		workerQueue.Run()
+		return nil
+	}, func(error) {
+		cancel()
+	})
+
+	// Start and listen on http
+	api := api.New()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: api,
+	}
+
+	g.Add(func() error {
+		return server.ListenAndServe()
+	}, func(error) {
+		server.Shutdown(ctx)
+	})
+
+	// Exit if we receieve SIGINT or SIGTERM
+	g.Add(func() error {
+		return handleInterrupt(ctx)
+	}, func(error) {
+		cancel()
+	})
+
+	log.Print(g.Run())
 }
