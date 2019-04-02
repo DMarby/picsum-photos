0cada134548be848426cfa30dfccc4cdc52b4026
David Marby <david@dmarby.se>
2019-04-02 06:00:56 +0200

    Explicitly order in postgres

--- database/postgresql/postgresql.go
+++ database/postgresql/postgresql.go
@@ -54,7 +54,7 @@ func (p *Provider) GetRandom() (id string, err error) {
 // ListAll returns a list of all the images
 func (p *Provider) ListAll() ([]database.Image, error) {
 	i := []database.Image{}
-	err := p.db.Select(&i, "select * from image")
+	err := p.db.Select(&i, "select * from image order by id")
 
 	if err != nil {
 		return nil, err
@@ -66,7 +66,7 @@ func (p *Provider) ListAll() ([]database.Image, error) {
 // List returns a list of all the images with an offset/limit
 func (p *Provider) List(offset, limit int) ([]database.Image, error) {
 	i := []database.Image{}
-	err := p.db.Select(&i, "select * from image OFFSET $1 LIMIT $2", offset, limit)
+	err := p.db.Select(&i, "select * from image order by id OFFSET $1 LIMIT $2", offset, limit)
 
 	if err != nil {
 		return nil, err
