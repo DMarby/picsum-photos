ed57a9533b2b9ee365961f5f66569dfdb8db9d5a
David Marby <david@dmarby.se>
2019-04-06 03:38:15 +0200

    Add flags for all the backends

--- main.go
+++ main.go
@@ -12,12 +12,19 @@ import (
 	"time"
 
 	"github.com/DMarby/picsum-photos/api"
-	memoryCache "github.com/DMarby/picsum-photos/cache/memory"
+	"github.com/DMarby/picsum-photos/cache"
+	"github.com/DMarby/picsum-photos/cache/memory"
+	"github.com/DMarby/picsum-photos/cache/redis"
+	"github.com/DMarby/picsum-photos/database"
 	fileDatabase "github.com/DMarby/picsum-photos/database/file"
+	"github.com/DMarby/picsum-photos/database/postgresql"
 	"github.com/DMarby/picsum-photos/health"
-	vipsProcessor "github.com/DMarby/picsum-photos/image/vips"
+	"github.com/DMarby/picsum-photos/image/vips"
 	"github.com/DMarby/picsum-photos/logger"
+	"github.com/DMarby/picsum-photos/storage"
 	fileStorage "github.com/DMarby/picsum-photos/storage/file"
+	"github.com/DMarby/picsum-photos/storage/spaces"
+
 	"github.com/jamiealquiza/envy"
 	"go.uber.org/zap"
 )
@@ -45,12 +52,43 @@ const (
 	staticPath   = "./static" // Path where the static files are located
 )
 
-func main() {
-	// Set up commandline flags
-	listen := flag.String("listen", ":8080", "listen address")
-	rootURL := flag.String("root-url", "https://picsum.photos", "root url")
-	loglevel := zap.LevelFlag("log-level", zap.InfoLevel, "log level (default \"info\") (debug, info, warn, error, dpanic, panic, fatal)")
+// Comandline flags
+var (
+	// Global
+	listen   = flag.String("listen", ":8080", "listen address")
+	rootURL  = flag.String("root-url", "https://picsum.photos", "root url")
+	loglevel = zap.LevelFlag("log-level", zap.InfoLevel, "log level (default \"info\") (debug, info, warn, error, dpanic, panic, fatal)")
 
+	// Storage
+	storageBackend = flag.String("storage", "file", "which storage backend to use (file, spaces)")
+
+	// Storage - File
+	storageFilePath = flag.String("storage-file-path", "./test/fixtures/file", "path to the file storage")
+
+	// Storage - Spaces
+	storageSpacesSpace     = flag.String("storage-spaces-space", "", "digitalocean space to use")
+	storageSpacesRegion    = flag.String("storage-spaces-region", "", "spaces region")
+	storageSpacesAccessKey = flag.String("storage-spaces-access-key", "", "spaces access key")
+	storageSpacesSecretKey = flag.String("storage-spaces-secret-key", "", "spaces secret key")
+
+	// Cache
+	cacheBackend = flag.String("cache", "memory", "which cache backend to use (memory, redis)")
+
+	// Cache - Redis
+	cacheRedisAddress  = flag.String("cache-redis-address", "redis://127.0.0.1:6379", "redis address, may contain authentication details")
+	cacheRedisPoolSize = flag.Int("cache-redis-pool-size", 10, "redis connection pool size")
+
+	// Database
+	databaseBackend = flag.String("database", "file", "which database backend to use (file, postgresql)")
+
+	// Database - File
+	databaseFilePath = flag.String("database-file-path", "./test/fixtures/file/metadata.json", "path to the database file")
+
+	// Database - Postgresql
+	databasePostgresqlAddress = flag.String("database-postgresql-address", "postgresql://postgres@127.0.0.1/postgres", "postgresql address")
+)
+
+func main() {
 	// Parse environment variables
 	envy.Parse("PICSUM")
 
@@ -69,30 +107,17 @@ func main() {
 	imageProcessorCtx, imageProcessorCancel := context.WithCancel(context.Background())
 	defer imageProcessorCancel()
 
-	imageProcessor, err := vipsProcessor.GetInstance(imageProcessorCtx, log)
+	imageProcessor, err := vips.GetInstance(imageProcessorCtx, log)
 	if err != nil {
 		log.Fatalf("error initializing image processor %s", err.Error())
 	}
 
-	// Initialize the storage
-	storage, err := fileStorage.New("./test/fixtures/file")
+	// Initialize the storage, cache and database
+	storage, cache, database, err := setupBackends()
 	if err != nil {
-		log.Fatalf("error initializing storage %s", err.Error())
-		return
+		log.Fatalf("error initializing backends: %s", err)
 	}
-
-	// Initialize the cache
-	cache := memoryCache.New()
 	defer cache.Shutdown()
-
-	imageCache := api.NewCache(cache, storage)
-
-	// Initialize the database
-	database, err := fileDatabase.New("./test/fixtures/file/metadata.json")
-	if err != nil {
-		log.Fatalf("error initializing database %s", err.Error())
-		return
-	}
 	defer database.Shutdown()
 
 	// Initialize and start the health checker
@@ -111,7 +136,7 @@ func main() {
 	// Start and listen on http
 	api := &api.API{
 		ImageProcessor: imageProcessor,
-		Cache:          imageCache,
+		Cache:          api.NewCache(cache, storage),
 		Database:       database,
 		HealthChecker:  checker,
 		Log:            log,
@@ -146,4 +171,46 @@ func main() {
 	if err := server.Shutdown(serverCtx); err != nil {
 		log.Warnf("error shutting down: %s", err)
 	}
+}
+
+func setupBackends() (storage storage.Provider, cache cache.Provider, database database.Provider, err error) {
+	// Storage
+	switch *storageBackend {
+	case "file":
+		storage, err = fileStorage.New(*storageFilePath)
+	case "spaces":
+		storage, err = spaces.New(*storageSpacesSpace, *storageSpacesRegion, *storageSpacesAccessKey, *storageSpacesSecretKey)
+	default:
+		err = fmt.Errorf("invalid storage backend")
+	}
+
+	if err != nil {
+		return
+	}
+
+	// Cache
+	switch *cacheBackend {
+	case "memory":
+		cache = memory.New()
+	case "redis":
+		cache, err = redis.New(*cacheRedisAddress, *cacheRedisPoolSize)
+	default:
+		err = fmt.Errorf("invalid cache backend")
+	}
+
+	if err != nil {
+		return
+	}
+
+	// Database
+	switch *databaseBackend {
+	case "file":
+		database, err = fileDatabase.New(*databaseFilePath)
+	case "postgresql":
+		database, err = postgresql.New(*databasePostgresqlAddress)
+	default:
+		err = fmt.Errorf("invalid database backend")
+	}
+
+	return
 }
