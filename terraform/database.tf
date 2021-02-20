cc3ba13bac21e964f36614889348be5ed214633e
David Marby <david@dmarby.se>
2021-02-20 14:54:48 +0100

    Remove the DO database

--- terraform/database.tf
+++
@@ -1,9 +0,0 @@
-resource "digitalocean_database_cluster" "picsum-db" {
-  name       = "picsum-db"
-  engine     = "pg"
-  version    = "11"
-  size       = "db-s-1vcpu-1gb"
-  region     = var.picsum_digitalocean_region
-  node_count = 1
-}
-
