519501eab0e4dfb1c421f46c2aa588d1e0b298a9
David Marby <david@dmarby.se>
2022-09-19 03:39:55 +0200

    Update k8s version

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "picsum_k8s" {
   name    = "picsum-k8s"
   region  = var.picsum_digitalocean_region
-  version = "1.17.5-do.0"
+  version = "1.24.4-do.0"
 
   node_pool {
     name       = "picsum-pool"
