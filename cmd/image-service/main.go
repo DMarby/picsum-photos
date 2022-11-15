6992dc46f008417a04fbaf46cb89c91af58f114e
David Marby <david@dmarby.se>
2022-11-15 08:47:04 +0100

    Remove cache settings for image service

--- cmd/image-service/main.go
+++ cmd/image-service/main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/DMarby/picsum-photos/internal/cache"
-	"github.com/DMarby/picsum-photos/internal/cache/memory"
 	"github.com/DMarby/picsum-photos/internal/cache/redis"
 	"github.com/DMarby/picsum-photos/internal/cmd"
 	"github.com/DMarby/picsum-photos/internal/health"
@@ -19,8 +16,6 @@ import (
 	"github.com/DMarby/picsum-photos/internal/image/vips"
 	"github.com/DMarby/picsum-photos/internal/logger"
 	"github.com/DMarby/picsum-photos/internal/metrics"
-	"github.com/DMarby/picsum-photos/internal/storage"
-	fileStorage "github.com/DMarby/picsum-photos/internal/storage/file"
 	"github.com/DMarby/picsum-photos/internal/storage/spaces"
 	"github.com/DMarby/picsum-photos/internal/tracing"
 
@@ -38,21 +33,12 @@ var (
 	metricsListen = flag.String("metrics-listen", ":8083", "metrics listen address")
 	loglevel      = zap.LevelFlag("log-level", zap.InfoLevel, "log level (default \"info\") (debug, info, warn, error, dpanic, panic, fatal)")
 
-	// Storage
-	storageBackend = flag.String("storage", "file", "which storage backend to use (file, spaces)")
-
-	// Storage - File
-	storageFilePath = flag.String("storage-file-path", "./test/fixtures/file", "path to the file storage")
-
 	// Storage - Spaces
 	storageSpacesSpace          = flag.String("storage-spaces-space", "", "digitalocean space to use")
 	storageSpacesEndpoint       = flag.String("storage-spaces-endpoint", "", "spaces endpoint")
 	storageSpacesForcePathStyle = flag.Bool("storage-spaces-force-path-style", false, "spaces force path style")
 	storageSpacesAccessKey      = flag.String("storage-spaces-access-key", "", "spaces access key")
 	storageSpacesSecretKey      = flag.String("storage-spaces-secret-key", "", "spaces secret key")
-
-	// Cache
-	cacheBackend = flag.String("cache", "memory", "which cache backend to use (memory, redis)")
 
 	// Cache - Redis
 	cacheRedisAddress  = flag.String("cache-redis-address", "redis://127.0.0.1:6379", "redis address, may contain authentication details")
@@ -96,9 +82,15 @@ func main() {
 	defer tracer.Shutdown(tracerCtx)
 
 	// Initialize the storage, cache
-	storage, cache, err := setupBackends(shutdownCtx, tracer)
+	storage, err := spaces.New(*storageSpacesSpace, *storageSpacesEndpoint, *storageSpacesAccessKey, *storageSpacesSecretKey, *storageSpacesForcePathStyle)
 	if err != nil {
-		log.Fatalf("error initializing backends: %s", err)
+		log.Fatalf("error initializing storage: %s", err)
+	}
+
+	// Initialize the cache
+	cache, err := redis.New(ctx, tracer, *cacheRedisAddress, *cacheRedisPoolSize)
+	if err != nil {
+		log.Fatalf("error initializing cache: %s", err)
 	}
 	defer cache.Shutdown()
 
@@ -156,32 +148,4 @@ func main() {
 	if err := server.Shutdown(serverCtx); err != nil {
 		log.Warnf("error shutting down: %s", err)
 	}
-}
-
-func setupBackends(ctx context.Context, tracer *tracing.Tracer) (storage storage.Provider, cache cache.Provider, err error) {
-	// Storage
-	switch *storageBackend {
-	case "file":
-		storage, err = fileStorage.New(*storageFilePath)
-	case "spaces":
-		storage, err = spaces.New(*storageSpacesSpace, *storageSpacesEndpoint, *storageSpacesAccessKey, *storageSpacesSecretKey, *storageSpacesForcePathStyle)
-	default:
-		err = fmt.Errorf("invalid storage backend")
-	}
-
-	if err != nil {
-		return
-	}
-
-	// Cache
-	switch *cacheBackend {
-	case "memory":
-		cache = memory.New()
-	case "redis":
-		cache, err = redis.New(ctx, tracer, *cacheRedisAddress, *cacheRedisPoolSize)
-	default:
-		err = fmt.Errorf("invalid cache backend")
-	}
-
-	return
 }
